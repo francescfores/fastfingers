4a0277f1522336a5cb2d6a79d2e5712c8433a392
francesc fores <ffores93@gmail.com>
2024-11-26 22:18:31 +0100

    refactoring

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,17 +17,7 @@ import {Router} from "@angular/router";
 })
 export class HomeComponent implements OnInit, AfterViewInit{
   projects: any[] = [];
-  words = ['can\'t', 'thing', 'sometimes', 'break', 'went', 'can\'t', 'been', 'question', 'quite', 'quit', 'from', 'form', 'forever', 'angry', 'limit', 'lucky', 'hours', 'records', 'oil', 'feed', 'won\'t', 'its', 'it\'s', 'old', 'many', 'place', 'time', 'experience', 'time', 'quest', 'just', 'computer', 'move', 'white', 'green', 'black', 'orange', 'different', 'hour', 'big', 'even', 'mountain', 'been', 'animal', 'long', 'after', 'page', 'tree', 'see', 'seen', 'right', 'left', 'has', 'went', 'above', 'said', 'good', 'long', 'it', 'why', 'that', 'learn', 'girl', 'boy', 'will', 'three', 'before', 'may', 'to', 'fine', 'flower', 'city', 'street'];
-  word:any;
-  letters:any;
-  letter:any;
-  time=10;
-  time_end=10;
 
-  intervalo: any;
-  word_index=0;
-  letter_index=0;
-  index=0;
 
   constructor(
     public themeService: ThemeService,
@@ -45,9 +35,8 @@ export class HomeComponent implements OnInit, AfterViewInit{
     }
   }
 
-    ngOnInit() {
+  ngOnInit() {
     this.projects = this.projectsService.projectsData;
-
     this.themeService.getCurrentTheme().subscribe(theme => {
       this.isDarkEnable = theme === 'theme-dark';
     });
@@ -61,49 +50,62 @@ export class HomeComponent implements OnInit, AfterViewInit{
       }, 1000);
   }
 
+
+  words = ['thing', 'sometimes', 'break', 'went', 'can\'t', 'been', 'question', 'quite', 'quit','hour', 'big', 'even', 'mountain', 'been', 'animal', 'long',];
+  word:any;
+  letters:any;
+  letter:any;
+  time=10;
+  time_end=10;
+  intervalo: any;
+  word_index=0;
+  letter_index=0;
+  index=0;
   keydown(event: KeyboardEvent) {
-  const key = event.key;
+    const key = event.key;
 
-  this.word = this.words[this.word_index];
-  this.letters= this.word.split('');
-  this.letter = this.letters[this.letter_index];
+    //obtener la palabra las letras de la palabra y la letra que se tiene que escribir
+    this.word = this.words[this.word_index];
+    this.letters = this.word.split("");
+    this.letter = this.letters[this.letter_index];
+    let letter;
 
-  let letter;
-
-  
-  letter = document.querySelectorAll('.word-letter')[this.index];
-  if (letter) {
-    if(key===this.letter){
-      letter.classList.add('bg-green-200');
-    }
-    letter.classList.remove('word-letter-anim');
-  }
-
-  if(this.letters.length  === this.letter_index+1){
-    if (event.code === 'Space') { // También puedes usar event.key === ' '
-        console.log('Se presionó la barra espaciadora.');
-        letter = document.querySelectorAll('.word-letter')[this.index];
-        letter.classList.remove('word-letter-anim-end');
-        this.word_index++
-        this.index++
-        this.letter_index=0;
-      const word = document.querySelectorAll('.word')[this.word_index];
-      letter = document.querySelectorAll('.word-letter')[this.letter_index+this.index];
-      letter.classList.add('word-letter-anim');
-
-    }else{
-      letter = document.querySelectorAll('.word-letter')[this.index];
+    //obtenemos la letra del DOM con el index que vamos incrementando
+    letter = document.querySelectorAll(".word-letter")[this.index];
     if (letter) {
-      letter.classList.add('word-letter-anim-end');
+      //si al escribir la letra es incorrecta pintamos la letra de rojo
+      if (key !== this.letter) {
+        letter.classList.add("text-red-300");
+      }
+      //borramos el | que se pinta con la clase word-letter-anim
+      letter.classList.remove("word-letter-anim");
     }
-    }
-    
-  }else{
-    this.letter_index++
-    this.index++
-    letter = document.querySelectorAll('.word-letter')[this.index];
-    if (letter) {
-      letter.classList.add('word-letter-anim');
+
+
+    if (this.letters.length !== this.letter_index + 1) {
+      this.letter_index++;
+      this.index++;
+      letter = document.querySelectorAll(".word-letter")[this.index];
+      if (letter) {
+        letter.classList.add("word-letter-anim");
+      }
+    } else {
+      if (event.code === "Space") {
+        // También puedes usar event.key === ' '
+        console.log("Se presionó la barra espaciadora.");
+        letter = document.querySelectorAll(".word-letter")[this.index];
+        letter.classList.remove("word-letter-anim-end");
+        this.word_index++;
+        this.index++;
+        this.letter_index = 0;
+        letter =
+          document.querySelectorAll(".word-letter")[this.letter_index + this.index];
+        letter.classList.add("word-letter-anim");
+      } else {
+        letter = document.querySelectorAll(".word-letter")[this.index];
+        if (letter) {
+          letter.classList.add("word-letter-anim-end");
+        }
       }
     }
   }
